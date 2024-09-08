40105621ed232fb00391add27c4ea3ec66a4937c
PetarGarmaz <petar.garmaz@gmail.com>
2024-09-08 14:35:27 +0200

    Fixed api briefing page limit

--- server/api/briefings/index.js
+++ server/api/briefings/index.js
@@ -8,17 +8,18 @@ export default defineEventHandler(async (event) => {
 			const host = new RegExp(params.searchQuery, "i");
 			const sort = params.orderBy == "Descending" ? "descending" : "ascending";
 			const filter = params.filterBy;
+			const limit = parseInt(params.pageLimit);
 			let data = [];
 			let dataCount = 0;
 
 			if(filter == 0) {
-				data = await BriefingSchema.find({$or: [{title}, {host}]}).lte("timestamp", nextWeek).populate({path:"creator"}).limit(10).skip((params.page - 1) * params.pageLimit).sort({timestamp : sort});
+				data = await BriefingSchema.find({$or: [{title}, {host}]}).lte("timestamp", nextWeek).populate({path:"creator"}).limit(limit).skip((params.page - 1) * params.pageLimit).sort({timestamp : sort});
 				dataCount = await BriefingSchema.countDocuments({$or: [{title}, {host}]}).lte("timestamp", nextWeek);
 			} else if(filter == 1) {
-				data = await BriefingSchema.find({$or: [{title}, {host}]}).gt("timestamp", nextWeek).populate({path:"creator"}).limit(10).skip((params.page - 1) * params.pageLimit).sort({timestamp : sort});
+				data = await BriefingSchema.find({$or: [{title}, {host}]}).gt("timestamp", nextWeek).populate({path:"creator"}).limit(limit).skip((params.page - 1) * params.pageLimit).sort({timestamp : sort});
 				dataCount = await BriefingSchema.countDocuments({$or: [{title}, {host}]}).gt("timestamp", nextWeek);
 			} else if(filter == 2) {
-				data = await BriefingSchema.find({$or: [{title}, {host}]}).populate({path:"creator"}).limit(10).skip((params.page - 1) * params.pageLimit).sort({timestamp : sort});
+				data = await BriefingSchema.find({$or: [{title}, {host}]}).populate({path:"creator"}).limit(limit).skip((params.page - 1) * params.pageLimit).sort({timestamp : sort});
 				dataCount = await BriefingSchema.countDocuments({$or: [{title}, {host}]});
 			}
 			
