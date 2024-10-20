d65230e0a959a349185180464b01ac57287d4713
PetarGarmaz <petar.garmaz@gmail.com>
2024-10-20 19:49:50 +0200

    Attempt to fix briefing not loading - 8

--- src/components/Briefing.vue
+++ src/components/Briefing.vue
@@ -11,6 +11,7 @@
 
 	const router = useRouter();
 	const briefingId = router.currentRoute._value.params.id;
+	const briefing = ref(null);
 
 	const getDateString = (timestamp) => {
 		const parsedDate = new Date(parseInt(timestamp));
@@ -26,13 +27,13 @@
 
 	const renderMarkdown = () => {
 		const md = new MarkdownIt().use(MarkdownItAttrs);
-		const rend = md.render(briefingStore.briefing.desc);
+		const rend = md.render(briefing.desc);
 		
 		return rend;
 	};
 
 	const handleProfileClick = () => {
-		router.push("/profile/" + briefingStore.briefing.creator?._id + "?name=" + briefingStore.briefing.creator?.username);
+		router.push("/profile/" + briefing.creator?._id + "?name=" + briefing.creator?.username);
 	};
 
 	const handleEdit = () => {
@@ -47,6 +48,7 @@
 	onMounted(() => {
 		const data = briefingStore.getBriefing(briefingId);
 		briefingStore.setBriefing(data);
+		briefing.value = data;
 	})
 </script>
 
@@ -54,26 +56,26 @@
 	<Observer>
 		<section class='min-h-screen'>
 			<Suspense>
-				<div v-if="briefingStore.briefing" class="flex flex-col w-9/12 mx-auto max-lg:w-11/12 bg-gray-900 my-16 border border-gray-600 rounded-lg">
-					<img v-bind:src="briefingStore.briefing.creator?.image" @click="handleProfileClick" alt="Logo" class='object-contain max-w-16 mx-auto my-2 rounded-full cursor-pointer' />
+				<div v-if="briefing" class="flex flex-col w-9/12 mx-auto max-lg:w-11/12 bg-gray-900 my-16 border border-gray-600 rounded-lg">
+					<img v-bind:src="briefing.creator?.image" @click="handleProfileClick" alt="Logo" class='object-contain max-w-16 mx-auto my-2 rounded-full cursor-pointer' />
 					
 					<hr class='border-gray-600'/>
-					<h1 class='text-5xl text-gray-200 font-bold tracking-widest text-center my-4 max-lg:mx-2 max-lg:text-3xl max-lg:tracking-normal'>{{briefingStore.briefing.title}}</h1>
+					<h1 class='text-5xl text-gray-200 font-bold tracking-widest text-center my-4 max-lg:mx-2 max-lg:text-3xl max-lg:tracking-normal'>{{briefing.title}}</h1>
 					
 					<hr class='border-gray-600'/>
-					<p class='text-gray-200 text-center max-lg:text-sm tracking-wider italic my-4 mx-4'>Host: {{briefingStore.briefing.host}} ● {{getDateString(briefingStore.briefing.timestamp)}} ● {{getTimeString(briefingStore.briefing.timestamp)}}</p>
+					<p class='text-gray-200 text-center max-lg:text-sm tracking-wider italic my-4 mx-4'>Host: {{briefing.host}} ● {{getDateString(briefing.timestamp)}} ● {{getTimeString(briefing.timestamp)}}</p>
 					
 					<hr class='border-gray-600 mt-1'/>
 					<hr class='border-gray-600 mt-1'/>
 					
-					<p v-if="briefingStore.briefing.desc" v-html="renderMarkdown()" class='briefing-text'></p>
+					<p v-if="briefing.desc" v-html="renderMarkdown()" class='briefing-text'></p>
 
-					<img v-if="briefingStore.briefing.image" v-bind:src="briefingStore.briefing.image" alt="briefing_image" class="m-5 rounded-lg"/>
+					<img v-if="briefing.image" v-bind:src="briefing.image" alt="briefing_image" class="m-5 rounded-lg"/>
 
 					<hr class='border-gray-600'/>
-					<p class='mx-5 my-2 text-2xl text-gray-200 font-bold'>STATUS: {{briefingStore.briefing.status}}</p>
+					<p class='mx-5 my-2 text-2xl text-gray-200 font-bold'>STATUS: {{briefing.status}}</p>
 
-					<div v-if="briefingStore.briefing.creator?._id == navbarStore.session?.user.id || navbarStore.session?.user.isAdmin">
+					<div v-if="briefing.creator?._id == navbarStore.session?.user.id || navbarStore.session?.user.isAdmin">
 						<hr class='border-gray-600'/>
 						<div class='flex flex-wrap'>
 							<button @click="handleEdit()" class='w-full mx-5 mt-5 text-center text-gray-200 bg-lime-700 border border-gray-600 hover:bg-lime-500 rounded-lg py-1 transition duration-300'>Edit Briefing</button>			
