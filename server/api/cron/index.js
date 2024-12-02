369a9c7ac90353088c40835c2e64fcc185e302f7
PetarGarmaz <petar.garmaz@gmail.com>
2024-12-02 22:21:08 +0100

    Fixed issues with cron job

--- server/api/cron/index.js
+++ server/api/cron/index.js
@@ -28,56 +28,54 @@ async function getAllBriefings() {
 	return timestamps;
 };
 
-export default defineEventHandler(async (event) => {
-	if (event.req.method == 'GET') {
-		const response = await authorize(event.req);
+export default async function eventHandler(req, res) {
+	const response = await authorize(req);
 
-		if (response) {
-			return new Response("Unauthorized", {status: 401});
-		} else {
-			const allBriefings = await getAllBriefings();
-			const emojis = ["one", "two", "three", "four", "five", "six", "seven"]; //7 available, just in case (literally not possible to have more than 5)
-			let currentDate = new Date();
-			var messageEmbeds = [];
-			var fridays = [];
-			
-			for (let i = currentDate.getDate(); i <= 31; i++) {
-				let newDate = new Date(currentDate);
-				newDate.setDate(i);
-				newDate.setUTCHours(19,0,0,0);
+	if (response) {
+		return new Response("Unauthorized", {status: 401});
+	} else {
+		const allBriefings = await getAllBriefings();
+		const emojis = ["one", "two", "three", "four", "five", "six", "seven"]; //7 available, just in case (literally not possible to have more than 5)
+		let currentDate = new Date();
+		var messageEmbeds = [];
+		var fridays = [];
+		
+		for (let i = currentDate.getDate(); i <= 31; i++) {
+			let newDate = new Date(currentDate);
+			newDate.setDate(i);
+			newDate.setUTCHours(19,0,0,0);
 
-				const newDateTimestamp = Math.round(newDate.getTime() / 1000);
+			const newDateTimestamp = Math.round(newDate.getTime() / 1000);
 
-				if(newDate.getDay() == 5 && !allBriefings.includes((newDateTimestamp * 1000).toString())) {
-					fridays.push(newDateTimestamp);
-				};
+			if(newDate.getDay() == 5 && !allBriefings.includes((newDateTimestamp * 1000).toString())) {
+				fridays.push(newDateTimestamp);
 			};
-
-			for (let i = 0; i <= fridays.length - 1; i++) {
-				const embed = {
-					"title": "",
-					"description": `:${emojis[i]}: :  <t:${fridays[i]}:F>`,
-					"color": 16711680,
-				};
-
-				messageEmbeds.push(embed);
-			};
-
-			const message = {
-				"attachments": [],
-				"content": "<@&598258350718713864>\n\nSchedule time!\n\nBelow you will find available mission slots this month. If you wish to schedule a mission [click here](https://a3.fugaming.org/create-briefing)!",
-				"embeds": messageEmbeds
-			};
-
-			await fetch(process.env.VITE_DISCORD_MISSIONS_WEBHOOK, {
-				headers: {
-					"Content-Type": "application/json",
-				},
-				method: "POST",
-				body: JSON.stringify(message),
-			});
-
-			return new Response("Nice!", {status: 200});
 		};
+
+		for (let i = 0; i <= fridays.length - 1; i++) {
+			const embed = {
+				"title": "",
+				"description": `:${emojis[i]}: :  <t:${fridays[i]}:F>`,
+				"color": 16711680,
+			};
+
+			messageEmbeds.push(embed);
+		};
+
+		const message = {
+			"attachments": [],
+			"content": "<@&598258350718713864>\n\nSchedule time!\n\nBelow you will find available mission slots this month. If you wish to schedule a mission [click here](https://a3.fugaming.org/create-briefing)!",
+			"embeds": messageEmbeds
+		};
+
+		await fetch(process.env.VITE_DISCORD_MISSIONS_WEBHOOK, {
+			headers: {
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify(message),
+		});
+
+		return new Response("Nice!", {status: 200});
 	};
-});
+};
